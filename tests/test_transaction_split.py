79c065b8e5af8d47e50e98dab207e12450ed3176
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-11-27 12:21:44 +0100

    Added testcase

--- tests/test_transaction_split.py
+++ tests/test_transaction_split.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from paystackpyAPI.transaction_splits import TransactionSplit
 from errors import APIError
+from unittest.mock import patch
 
 import unittest
 from os import getenv
@@ -33,3 +34,30 @@ class TestPaystackAPISplit(unittest.TestCase):
             self.assertEqual(res["response_from_api"]["message"], "Split created")
         finally:
             self.api.close()
+
+    @patch('paystackpyAPI.transaction_splits.requests.post')
+    def test_transaction_split_mock(self, mock_post):
+        mock_post.return_value.status_code = 400
+        mock_post.return_value.text = '{"status": false, "message": "Bearer subaccount must be part of split group"}'
+
+        split_name = "Test Split"
+        split_type = "percentage"
+        split_currency = "NGN"
+        split_subaccounts = [
+            {"subaccount": "2333", "share": 20}
+        ]
+        split_bearer_type = "subaccount"
+        split_bearer_subaccount = "ACCT"
+
+        with self.assertRaises(APIError) as context:
+            self.api.create_split(split_name, split_type, split_currency,
+                                  split_subaccounts, split_bearer_type, split_bearer_subaccount)
+
+        self.assertEqual(context.exception.status_code, 400)
+        self.assertEqual(context.exception.error_message, mock_post.return_value.text)
+
+        self.api.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
